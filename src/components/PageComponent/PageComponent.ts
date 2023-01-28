99bca89892d98ed35852d9e4f77909f5c1b27cd5
Maximiliano Gutierrez <hmgutierrezfernandez@gmail.com>
2023-01-28 22:11:49 +0100

    Add image to HeaderComponen and upload PageComponent

---
+++ src/components/PageComponent/PageComponent.ts
@@ -0,0 +1,9 @@
+import Component from "../Component/Component.js";
+
+class PageComponent extends Component {
+  constructor(parentElement: Element) {
+    super(parentElement, "main-page", "main");
+  }
+}
+
+export default PageComponent;
